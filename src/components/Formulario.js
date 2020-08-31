31d8614ef7dba128d9afc2e044a73fc29c0df764
Tirso del Álamo <tirsodelalamomartin@gmail.com>
2020-08-31 12:29:50 +0200

    Done

---
+++ src/components/Formulario.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from './Formulario.module.css'
+import PropTypes from 'prop-types'
+import useSelect from '../Hooks/useSelect'
+
+const Formulario = ({guardarCategoria}) => {
+
+    const OPCIONES = [
+      { value: "general", label: "General" },
+      { value: "business", label: "Negocios" },
+      { value: "entertainment", label: "Entretenimiento" },
+      { value: "health", label: "Salud" },
+      { value: "science", label: "Ciencia" },
+      { value: "sports", label: "Deportes" },
+      { value: "technology", label: "Tecnología" },
+    ];
+
+    //Utilizar Custom Hook
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES)
+
+    //Submit al form, pasar categoría a app.js
+    const buscarNoticias = e => {
+        e.preventDefault()
+
+        guardarCategoria(categoria)
+    }
+
+    return ( 
+        <div className={`${styles.buscador} row`}>
+            <div className='col s12 m8 offset-m2'>
+                <form onSubmit={buscarNoticias}>
+                    <h2 className={styles.heading}>Encuentra noticias por Categoría</h2>
+
+                    <SelectNoticias/>
+
+                    <div className='input-field col s12'>
+                        <input type='submit' className={`${styles.btn_block} btn-large amber darken-2`} value='Buscar'/>
+                    </div>
+                </form>
+            </div>
+        </div>
+     );
+}
+
+Formulario.propTypes = {
+  guardarCategoria: PropTypes.func.isRequired
+};
+ 
+export default Formulario;
